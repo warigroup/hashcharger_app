a790cd4d10e81cba0887b7f12e09f31ddeb11923
webdevstorage <46852738+webdevstorage@users.noreply.github.com>
2020-04-06 15:17:38 -0400

    config

--- server.js
+++ server.js
@@ -3,13 +3,16 @@ const next = require('next')
 const routes = require('./routes')
 const app = next({dev: process.env.NODE_ENV !== 'production'})
 const handler = routes.getRequestHandler(app)
-const cors = require('cors')
 
 // With express
 const express = require('express')
-const expressApp = express();
-expressApp.use(cors())
+
+var allowCrossDomain = function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*"); // allow requests from any other server
+  res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE'); // allow these verbs
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Cache-Control");
+}  
 
 app.prepare().then(() => {
-  expressApp.use(handler).listen(3000)
+  express().use(handler, allowCrossDomain).listen(3000)
 })
