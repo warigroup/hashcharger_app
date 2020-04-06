dbfadd219b02a5c71b566447adeaa4a3c1e2771a
webdevstorage <46852738+webdevstorage@users.noreply.github.com>
2020-04-06 15:01:19 -0400

    add cors

--- server.js
+++ server.js
@@ -3,9 +3,13 @@ const next = require('next')
 const routes = require('./routes')
 const app = next({dev: process.env.NODE_ENV !== 'production'})
 const handler = routes.getRequestHandler(app)
+const cors = require('cors')
 
 // With express
 const express = require('express')
+const expressApp = express();
+expressApp.use(cors())
+
 app.prepare().then(() => {
-  express().use(handler).listen(3000)
+  expressApp.use(handler).listen(3000)
 })
