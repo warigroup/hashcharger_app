7df426cb8c4acc7fc677abc21e0b385011b53dfb
webdevstorage <46852738+webdevstorage@users.noreply.github.com>
2020-04-06 16:32:37 -0400

    update2

--- server.js
+++ server.js
@@ -5,11 +5,7 @@ const app = next({dev: process.env.NODE_ENV !== 'production'})
 const handler = routes.getRequestHandler(app)
 // With express
 const express = require('express')
-const helmet = require('helmet')
-
-const expressApp = express();
-expressApp.use(helmet())
 
 app.prepare().then(() => {
-  express().use(handler, helmet.frameguard()).listen(3000)
+  express().use(handler).listen(3000)
 })
