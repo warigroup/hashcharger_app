c5d9005e690b25d1a921762275542a061db78f4e
webdevstorage <46852738+webdevstorage@users.noreply.github.com>
2020-04-06 17:30:33 -0400

    test2

--- server.js
+++ server.js
@@ -5,18 +5,25 @@ const app = next({dev: process.env.NODE_ENV !== 'production'})
 const handler = routes.getRequestHandler(app)
 // With express
 const express = require('express')
+const helmet = require('helmet');
 
-var enableCORS = function(req, res, next) {
-  res.header('Access-Control-Allow-Origin', '*');
-  res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
-  res.header('Access-Control-Allow-Headers', 'Content-Type, token, Content-Length, X-Requested-With, *');
-  if ('OPTIONS' === req.method) {
-    res.sendStatus(200);
-  } else {
-    next();
+// var enableCORS = function(req, res, next) {
+//   res.header('Access-Control-Allow-Origin', '*');
+//   res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
+//   res.header('Access-Control-Allow-Headers', 'Content-Type, token, Content-Length, X-Requested-With, *');
+//   if ('OPTIONS' === req.method) {
+//     res.sendStatus(200);
+//   } else {
+//     next();
+//   }
+// };
+
+express().use(helmet({
+  frameguard: {
+    action: 'deny'
   }
-};
-express().use(enableCORS);
+}))
+
 
 app.prepare().then(() => {
   express().use(handler).listen(3000)
