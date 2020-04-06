3e5366ea838d56118dedd8f572e4a54200bce46e
webdevstorage <46852738+webdevstorage@users.noreply.github.com>
2020-04-06 16:44:21 -0400

    helmet

--- server.js
+++ server.js
@@ -3,8 +3,13 @@ const next = require('next')
 const routes = require('./routes')
 const app = next({dev: process.env.NODE_ENV !== 'production'})
 const handler = routes.getRequestHandler(app)
+const helmet = require('helmet')
 // With express
 const express = require('express')
+
+express().use(helmet({
+  frameguard: false
+}));
 
 app.prepare().then(() => {
   express().use(handler).listen(3000)
