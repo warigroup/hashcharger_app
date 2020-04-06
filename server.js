f975edf71876f9dc59ba96de7f453a7938dd8cfc
webdevstorage <46852738+webdevstorage@users.noreply.github.com>
2020-04-06 16:12:58 -0400

    add helmet

--- server.js
+++ server.js
@@ -5,16 +5,11 @@ const app = next({dev: process.env.NODE_ENV !== 'production'})
 const handler = routes.getRequestHandler(app)
 // With express
 const express = require('express')
-var xFrameOptions = require('x-frame-options')
-var allowCrossDomain = function(req, res, next) {
-  res.header("Access-Control-Allow-Origin", "*"); // allow requests from any other server
-  res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE'); // allow these verbs
-  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Cache-Control");
-}  
+const helmet = require('helmet')
 
-
+const expressApp = express();
+expressApp.use(helmet())
 
 app.prepare().then(() => {
-  express().use(allowCrossDomain);
-  express().use(handler).listen(3000)
+  express().use(handler, helmet.frameguard()).listen(3000)
 })
