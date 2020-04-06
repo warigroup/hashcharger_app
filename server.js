e6f0f4e8b9fef3a665058e0f4c2431f8ecae70e7
webdevstorage <46852738+webdevstorage@users.noreply.github.com>
2020-04-06 15:58:35 -0400

    update2

--- server.js
+++ server.js
@@ -3,16 +3,16 @@ const next = require('next')
 const routes = require('./routes')
 const app = next({dev: process.env.NODE_ENV !== 'production'})
 const handler = routes.getRequestHandler(app)
-const xFrameOptions = require('x-frame-options')
 // With express
 const express = require('express')
-
+var xFrameOptions = require('x-frame-options')
 var allowCrossDomain = function(req, res, next) {
   res.header("Access-Control-Allow-Origin", "*"); // allow requests from any other server
   res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE'); // allow these verbs
   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Cache-Control");
 }  
+express().use(xFrameOptions());
 
 app.prepare().then(() => {
-  express().use(handler, xFrameOptions()).listen(3000)
+  express().use(handler).listen(3000)
 })
