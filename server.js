63d24f6baac1d3e77fc1879df1c40bfc732c75e3
webdevstorage <46852738+webdevstorage@users.noreply.github.com>
2020-04-06 17:20:45 -0400

    express

--- server.js
+++ server.js
@@ -6,7 +6,17 @@ const handler = routes.getRequestHandler(app)
 // With express
 const express = require('express')
 
-
+var enableCORS = function(req, res, next) {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
+  res.header('Access-Control-Allow-Headers', 'Content-Type, token, Content-Length, X-Requested-With, *');
+  if ('OPTIONS' === req.method) {
+    res.sendStatus(200);
+  } else {
+    next();
+  }
+};
+express().use(enableCORS);
 
 app.prepare().then(() => {
   express().use(handler).listen(3000)
