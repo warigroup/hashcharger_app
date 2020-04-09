09cb9387820c6514dc95b300aecb065962007775
webdevstorage <46852738+webdevstorage@users.noreply.github.com>
2020-04-09 14:24:42 -0400

    revert1

--- pages/marketplace.js
+++ pages/marketplace.js
@@ -21,6 +21,7 @@ import {
 import { FaRegClock, FaBitcoin, FaQuestionCircle } from "react-icons/fa";
 import { TiFlash } from "react-icons/ti";
 import { Router } from "../routes";
+import MiningAlgoDropDown from "../components/tools/MiningAlgoDropDown";
 import PropTypes from "prop-types";
 import NProgress from "nprogress";
 import CSRFToken from "../utils/csrftoken";
@@ -722,7 +723,10 @@ class Marketplace extends React.Component {
              <div className="col-xl-12 col-lg-12 col-md-12 col-sm-12 col-12"> 
               {/******* MINING ALGORITHM SELECTOR *********/}
                <div className="miningalgo-selector-container">
-                  <h4 className="marketplacetitle">Buy Hashing Power</h4>
+                  <h4 className="marketplacetitle">Buy Hashing Power for</h4>
+                  <MiningAlgoDropDown 
+                    selectAlgorithm={this.selectAlgorithm}
+                    />
                    
                 </div>
                 {/******* MINING ALGORITHM SELECTOR END *********/}
