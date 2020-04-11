a1b1939e87081c528bf62a80417364d2da4c5268
webdevstorage <46852738+webdevstorage@users.noreply.github.com>
2020-04-10 23:12:01 -0400

    remove space

--- pages/marketplace.js
+++ pages/marketplace.js
@@ -773,10 +773,10 @@ class Marketplace extends React.Component {
 <div className="container">
 <div className="row">
            <div className="col-xl-12 col-lg-12 col-md-12 col-sm-12 col-12 text-center">
-                <br /><br /><br /><br />
+                <br /><br />
                 <div className="container">
                    <div className="row">
-                     <div className="col-xl-5 col-lg-5 col-md-12 col-sm-12 col-12 text-center">
+                     <div className="col-xl-12 col-lg-12 col-md-12 col-sm-12 col-12 text-center">
                        <img src="/static/power-off-icon-2.svg" 
                        className="nopower-icon" alt="Not available" />
 
@@ -785,13 +785,11 @@ class Marketplace extends React.Component {
                 </div>
                 <br />
                 <br />
-                <br />
-      
               <div>
               <p style={{fontWeight: "bold",
               fontSize: "0.85em"}}>Currently, no hashing power is available for this algorithm. Please check again later.
               <br /> You can still buy hashing power for different mining algorithms.</p>
-              <br /><br />
+           
               </div> 
            </div>
        </div>
