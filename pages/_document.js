b6759e32a2be8db94eec9f85a66e8f847c94a24e
webdevstorage <46852738+webdevstorage@users.noreply.github.com>
2020-04-13 22:08:20 -0400

    fix

--- pages/_document.js
+++ pages/_document.js
@@ -40,7 +40,7 @@ class MyDocument extends Document {
             name="viewport"
             content="width=device-width, initial-scale=1, shrink-to-fit=no, user-scalable=no"
           />
-          <title>WariHash</title>
+          
           <meta name="description" content="" />
           <meta name="keywords" content="" />
           <meta name="author" content="" />
