701bb5eb515bc93d329dfd9baf62eac9b9783ded
webdevstorage <46852738+webdevstorage@users.noreply.github.com>
2020-04-14 23:52:23 -0400

    set min height

--- components/routes/PublicRoute.js
+++ components/routes/PublicRoute.js
@@ -1,7 +1,8 @@
 import MarketNav from "../common/MarketNav";
 
 const PublicRoute = ({ children }) => (
-  <main style={{padding: "0px", position: "relative"}}>
+  <main 
+  style={{ padding: "0px", position: "relative", minHeight: "100vh" }}>
     {children}
     
     <MarketNav />
