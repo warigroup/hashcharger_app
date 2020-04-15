50b539321a80cea9b451e80636aff62b31cccf30
webdevstorage <46852738+webdevstorage@users.noreply.github.com>
2020-04-14 23:43:43 -0400

    fix url

--- routes.js
+++ routes.js
@@ -1,7 +1,7 @@
 const routes = require('next-routes')
 
 module.exports = routes()
-.add('marketplace', '/:stratumaddress?/:port?')
+.add('marketplace', '/market/:stratumaddress?/:port?')
 .add('invoice', '/invoice/id/:bidid')
 .add('orderdetails', '/orderdetails/id/:bidid')
 .add('search', '/search')
