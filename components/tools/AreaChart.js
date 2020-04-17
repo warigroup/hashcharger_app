4c6ab4a9aa5afe3736f8759e38e137c0ee6d151c
webdevstorage <46852738+webdevstorage@users.noreply.github.com>
2020-04-17 19:52:27 -0400

    update

--- components/tools/AreaChart.js
+++ components/tools/AreaChart.js
@@ -1,5 +1,5 @@
 import { Line } from "react-chartjs-2";
-import React, { Component } from 'react'
+import React, { Component } from 'react';
 import moment from "moment";
 
 class AreaChart extends Component {
