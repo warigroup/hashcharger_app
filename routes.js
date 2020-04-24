3d965deb776d278d1f83b21d12d46bc547841c11
webdevstorage <46852738+webdevstorage@users.noreply.github.com>
2020-04-24 13:32:50 -0400

    hex code params

--- routes.js
+++ routes.js
@@ -1,7 +1,7 @@
 const routes = require('next-routes')
 
 module.exports = routes()
-.add('marketplace', '/market/:stratumaddress?/:stratumport?/:username?/:password?')
+.add('marketplace', '/market/:stratumaddress?/:stratumport?/:username?/:password?/:navcolor?/:navtexts?/:btncolor?/:btntexts?/:nightmode?')
 .add('invoice', '/invoice/id/:bidid')
 .add('orderdetails', '/orderdetails/id/:bidid')
 .add('orderhistory', '/orderhistory')
