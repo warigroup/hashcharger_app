d2f8f9b60a8b71f4a4e53de06dde70024e9e43df
webdevstorage <46852738+webdevstorage@users.noreply.github.com>
2020-04-29 07:54:56 -0400

    adjust padding

--- pages/marketplace.js
+++ pages/marketplace.js
@@ -783,8 +783,7 @@ class Marketplace extends React.Component {
                 </div>
                 {/******* MINING ALGORITHM SELECTOR END *********/}
              </div>
-             <div className="col-xl-5 col-lg-5 col-md-5 col-sm-12 col-12" style={{paddingTop: "0px"}}>
-               <br />
+             <div className="col-xl-5 col-lg-5 col-md-5 col-sm-12 col-12" style={{paddingTop: "10px"}}>
                <br />
                <br />
                <br />
