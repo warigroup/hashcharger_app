134458412fc4e8c7db85d2e75665bf6df0e6fe32
webdevstorage <46852738+webdevstorage@users.noreply.github.com>
2020-04-28 23:15:28 -0400

    change length

--- utils/alphaNumericCheck.js
+++ utils/alphaNumericCheck.js
@@ -1,6 +1,6 @@
 function alphaNumericCheck(inputtxt) {
 	var letters = /^[0-9a-zA-Z]+$/;
-	if (inputtxt.value.match(letters) && inputtxt.length < 10 && inputtxt.length > 2) {
+	if (inputtxt.value.match(letters) && inputtxt.length < 13 && inputtxt.length > 2) {
 		return true;
 	} else {
 		return false;
