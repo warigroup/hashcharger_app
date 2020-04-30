d9cb452ba0b16ba9a64ba4f14538d0df3f6ca5f6
webdevstorage <46852738+webdevstorage@users.noreply.github.com>
2020-04-30 01:06:20 -0400

    left

--- pages/marketplace.js
+++ pages/marketplace.js
@@ -1228,7 +1228,7 @@ class Marketplace extends React.Component {
                         paddingTop: "0px" }}>
                         <div className="col-xl-7 col-lg-7 col-md-12 col-sm-12 col-12 text-right"
                           style={{ marginTop: "8px", marginLeft: "0px", 
-                          paddingLeft: "0%", position: "relative", left: "20px"}}>
+                          paddingLeft: "0%", position: "relative", left: "60px"}}>
                       <label>
                         <span
                           style={{
