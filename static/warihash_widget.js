0fe073a1da763805404a7227f80f11680e588be0
webdevstorage <46852738+webdevstorage@users.noreply.github.com>
2020-04-29 23:09:11 -0400

    Update warihash_widget.js

--- static/warihash_widget.js
+++ static/warihash_widget.js
@@ -29,6 +29,7 @@ var createModal = function() {
     var navtexts = (myScript.getAttribute('navtexts') || '');
     var btncolor = (myScript.getAttribute('btncolor') || '');
     var btntexts = (myScript.getAttribute('btntexts') || '');
+    var closebtn = (myScript.getAttribute('closebtn') || '');
     var nightmode = (myScript.getAttribute('nightmode') || '');
 
     // modal window div
@@ -55,6 +56,7 @@ var createModal = function() {
     widgetCloseBtn.id = 'warihash-close-button';
     widgetCloseBtn.title = 'Close window';
     widgetCloseBtn.innerHTML = '<h4>&times;</h4>';
+    widgetCloseBtn.style.color = closebtn;
 
     // add widget close function
     widgetCloseBtn.addEventListener("click", function () {
