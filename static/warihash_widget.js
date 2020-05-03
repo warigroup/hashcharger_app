ef1b31b6a2e137c7bbbde5c9a4ce7b221bc806b0
webdevstorage <46852738+webdevstorage@users.noreply.github.com>
2020-05-03 18:26:18 -0400

    svg icon

--- static/warihash_widget.js
+++ static/warihash_widget.js
@@ -56,7 +56,7 @@ var createModal = function() {
     widgetCloseBtn.className = 'warihash-close-button';
     widgetCloseBtn.id = 'warihash-close-button';
     widgetCloseBtn.title = 'Close window';
-    widgetCloseBtn.innerHTML = '<h4>&times;</h4>';
+    widgetCloseBtn.innerHTML = '<svg height="20px" version="1.1" viewBox="0 0 14 14" width="20px" xmlns="http://www.w3.org/2000/svg" xmlns:sketch="http://www.bohemiancoding.com/sketch/ns" xmlns:xlink="http://www.w3.org/1999/xlink"><title/><desc/><defs/><g fill="none" fill-rule="evenodd" id="Page-1" stroke="none" stroke-width="1"><g fill="#000000" id="Core" transform="translate(-341.000000, -89.000000)"><g id="close" transform="translate(341.000000, 89.000000)"><path d="M14,1.4 L12.6,0 L7,5.6 L1.4,0 L0,1.4 L5.6,7 L0,12.6 L1.4,14 L7,8.4 L12.6,14 L14,12.6 L8.4,7 L14,1.4 Z" id="Shape"/></g></g></g></svg>';
     widgetCloseBtn.style.color = closebtn;
 
     // add widget close function
