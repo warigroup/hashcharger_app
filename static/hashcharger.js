b941aa27a0727d8a81484e067338082169e31d86
webdevstorage <46852738+webdevstorage@users.noreply.github.com>
2020-05-08 00:37:07 -0400

    change class name

--- static/hashcharger.js
+++ static/hashcharger.js
@@ -1,4 +1,4 @@
-// add CSS
+
 var cssId = 'myCss';  
 if (!document.getElementById(cssId)) {
    var head  = document.getElementsByTagName('head')[0];
@@ -6,7 +6,6 @@ if (!document.getElementById(cssId)) {
    link.id   = cssId;
    link.rel  = 'stylesheet';
    link.type = 'text/css';
-   // use cloudflare CDN
    link.href = 'https://app.warihash.org/static/hashcharger.css';
    link.media = 'all';
    head.appendChild(link);
@@ -18,7 +17,7 @@ var widgetOpenBtn = document.getElementsByClassName("open-hashcharger");
 
 var createModal = function() {
     document.body.classList.add("disable-scroll-overflow");
-    // get data param
+
     var myScript = document.getElementById('hashcharger');
     var host = (myScript.getAttribute('host') || '');
     var port = (myScript.getAttribute('port') || '');
@@ -33,13 +32,11 @@ var createModal = function() {
     var tabletexts = (myScript.getAttribute('tabletexts') || '');
     var fullscreen = (myScript.getAttribute('fullscreen') || '');
 
-    // modal window div
     var modalWindow = document.createElement('div');
-    modalWindow.className = 'warihash-widget';
-    modalWindow.id = 'warihash-widget';
+    modalWindow.className = 'hashcharger-widget';
+    modalWindow.id = 'hashcharger-widget';
     document.getElementsByTagName('body')[0].appendChild(modalWindow);
 
-    // append iframe to modalWindow
     var iframe = document.createElement('iframe');
     // eslint-disable-next-line no-useless-concat
     iframe.src = 'https://app.warihash.org/market' + '/' + host + '/' + port + '/' + 
@@ -51,19 +48,16 @@ var createModal = function() {
     iframe.sandbox = 'allow-popups allow-popups-to-escape-sandbox allow-forms allow-same-origin allow-scripts allow-top-navigation allow-top-navigation-by-user-activation';
     modalWindow.appendChild(iframe);
 
-    // append close button to modalwindow
     var widgetCloseBtn = document.createElement('button');
     widgetCloseBtn.type = 'button';
-    widgetCloseBtn.className = 'warihash-close-button';
-    widgetCloseBtn.id = 'warihash-close-button';
+    widgetCloseBtn.className = 'hashcharger-close-button';
+    widgetCloseBtn.id = 'hashcharger-close-button';
     widgetCloseBtn.title = 'Close window';
     widgetCloseBtn.innerHTML = '<svg class="svg-close-icon" enable-background="new 0 0 24 24" id="Layer_1" version="1.1" viewBox="0 0 24 24"><path d="M22.245,4.015c0.313,0.313,0.313,0.826,0,1.139l-6.276,6.27c-0.313,0.312-0.313,0.826,0,1.14l6.273,6.272  c0.313,0.313,0.313,0.826,0,1.14l-2.285,2.277c-0.314,0.312-0.828,0.312-1.142,0l-6.271-6.271c-0.313-0.313-0.828-0.313-1.141,0  l-6.276,6.267c-0.313,0.313-0.828,0.313-1.141,0l-2.282-2.28c-0.313-0.313-0.313-0.826,0-1.14l6.278-6.269  c0.313-0.312,0.313-0.826,0-1.14L1.709,5.147c-0.314-0.313-0.314-0.827,0-1.14l2.284-2.278C4.308,1.417,4.821,1.417,5.135,1.73  L11.405,8c0.314,0.314,0.828,0.314,1.141,0.001l6.276-6.267c0.312-0.312,0.826-0.312,1.141,0L22.245,4.015z"/></svg>';
 
-    // add widget close function
     widgetCloseBtn.addEventListener("click", function () {
-        var elem = document.getElementById('warihash-widget');
+        var elem = document.getElementById('hashcharger-widget');
         elem.classList.add('widget-fadeout');
-        // remove modal after 0.45 sec
         setTimeout(function(){ 
             elem.parentNode.removeChild(elem); 
             document.body.classList.remove("disable-scroll-overflow");
