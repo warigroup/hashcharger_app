099e704070cc9be1e6c460d99423c30a72b196c5
webdevstorage <46852738+webdevstorage@users.noreply.github.com>
2020-05-14 13:14:55 -0400

    add classes

--- components/routes/PublicRoute.js
+++ components/routes/PublicRoute.js
@@ -1,13 +1,15 @@
 import MarketNav from "../common/MarketNav";
 
 const PublicRoute = ({ children }) => (
-  <main 
+  <main className="maincontainer1"
   style={{ padding: "0px", position: "relative", minHeight: "100vh" }}>
+    <div className="maincontainer2">
     {children}
     
     <br />
     <br />
     <MarketNav />
+    </div>
   </main>
 );
 
