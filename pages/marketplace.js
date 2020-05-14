e8ea58c35b59efb98742162c21f69db60c0b57b7
webdevstorage <46852738+webdevstorage@users.noreply.github.com>
2020-05-14 12:19:06 -0400

    style fix

--- pages/marketplace.js
+++ pages/marketplace.js
@@ -694,7 +694,7 @@ class Marketplace extends React.Component {
 
             .limit-price-container {
               margin-top: 8px; 
-              margin-left: 20px; 
+              margin-left: 0px; 
               padding-left: 0px; 
               position: relative; 
             }
