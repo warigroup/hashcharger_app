297253c61824d2b6fd7c9ff90e47d9e92d5ad303
webdevstorage <46852738+webdevstorage@users.noreply.github.com>
2020-05-14 13:02:21 -0400

    style fix

--- pages/orderhistory.js
+++ pages/orderhistory.js
@@ -234,7 +234,7 @@ class myOrderHistory extends React.Component {
 
                   <div className="col-md-12 clearfix mb-2" />
                   <div className="col-xl-12 col-lg-12 col-md-12 col-sm-12 col-12 tablecontainer" 
-                  style={{marginBottom: "0px", paddingBottom: "0px"}}>
+                  style={{paddingBottom: "0px"}}>
                     <div className="buy-orders-table" 
                     style={{width: "100%", marginBottom: "35px", paddingBottom: "0px"}}>
                     <div className="board" style={{ borderBottom: "1px solid rgba(0,0,0,0.3)", borderRadius: "0px" }}>
