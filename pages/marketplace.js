71124dd8552fe0c367776ed8d85055d901b88902
webdevstorage <46852738+webdevstorage@users.noreply.github.com>
2020-05-28 14:42:04 -0400

    fix

--- pages/marketplace.js
+++ pages/marketplace.js
@@ -103,8 +103,9 @@ class Marketplace extends React.Component {
 
     // select algorithm
     if ( this.props.algorithm !== undefined && algorithms.includes(this.props.algorithm) === true ) {
-      this.selectAlgorithm(this.props.algorithm);
+      
       setTimeout(() => {
+        this.selectAlgorithm(this.props.algorithm);
         this.selectFirstRegion(this.props.algorithm);
       }, 1450)
     };
