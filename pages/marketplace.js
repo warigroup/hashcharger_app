4f74bca7b88435a1d4c45069c4b58562035da1ba
webdevstorage <46852738+webdevstorage@users.noreply.github.com>
2020-05-28 14:38:11 -0400

    fix3

--- pages/marketplace.js
+++ pages/marketplace.js
@@ -103,9 +103,13 @@ class Marketplace extends React.Component {
 
     // select algorithm
     if ( this.props.algorithm !== undefined && algorithms.includes(this.props.algorithm) === true ) {
-      this.selectAlgorithm(this.props.algorithm);
+      this.props.algoSelect(this.props.algorithm);
+      const hashunits = ((this.props.configs || {})[this.props.algorithm] || {}).hashrate_units;
+      this.setState({ mining_algo: this.props.algorithm, hashrate_units: hashunits });
+      Cookies.set("algo_select", this.props.algorithm, { expires: 7 });
+      this.setState({ durationClicked: false });
       setTimeout(() => {
-        this.selectFirstRegion(algorithm_name);
+        this.selectFirstRegion(this.props.algorithm);
       }, 1300)
     };
 
