f2f6c065d83a1f07fd15431191ee9ebb38bb8e36
webdevstorage <46852738+webdevstorage@users.noreply.github.com>
2020-06-04 19:50:37 -0400

    left

--- pages/marketplace.js
+++ pages/marketplace.js
@@ -493,7 +493,7 @@ class Marketplace extends React.Component {
             margin-left: 0px; 
             padding-left: 0%; 
             position: relative; 
-            left: 20px;
+            left: 50px;
             top: 6px;
           }
 
