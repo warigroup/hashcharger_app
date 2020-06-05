6a3ea8389447faeb5d140a980fba4bdf2ac210ef
webdevstorage <46852738+webdevstorage@users.noreply.github.com>
2020-06-04 20:57:10 -0400

    Update marketplace.js

--- pages/marketplace.js
+++ pages/marketplace.js
@@ -1508,8 +1508,8 @@ class Marketplace extends React.Component {
                     </div>
 
       <div className="offset-xl-6 col-xl-5 col-lg-12 col-md-12 col-12 text-xl-right text-lg-left text-md-left text-left"
-      style={{paddingLeft: "55px", paddingRight: "40px"}}>
-           <div style={{borderTop: "1px solid rgba(0,0,0,0.5)", paddingTop: "16px"}}>
+      style={{paddingLeft: "60px", paddingRight: "43px"}}>
+           <div style={{borderTop: "1px solid rgba(0,0,0,0.3)", paddingTop: "16px"}}>
                       <div className="container-fluid">
                         <div className="row">
                             <div className="col-xl-6 col-lg-6 col-md-6 col-sm-6 col-6" style={{paddingLeft: "0px"}}>
