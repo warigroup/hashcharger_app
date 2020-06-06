8d7ab5773ec7eea81df8efaa87d5a5a11d1f9c53
webdevstorage <46852738+webdevstorage@users.noreply.github.com>
2020-06-05 20:16:41 -0400

    fix left

--- pages/marketplace.js
+++ pages/marketplace.js
@@ -769,7 +769,7 @@ class Marketplace extends React.Component {
             @media (max-width: 770px) {
               .limit-price-container {
                 position: relative; 
-                left: 3px;
+                left: -10px;
               }
 
               .refund-address-container {
