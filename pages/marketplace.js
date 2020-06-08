6d500e3845042e8cc367bb92d04e8c6beeda189b
webdevstorage <46852738+webdevstorage@users.noreply.github.com>
2020-06-08 11:17:21 -0400

    fix style

--- pages/marketplace.js
+++ pages/marketplace.js
@@ -1553,8 +1553,8 @@ class Marketplace extends React.Component {
                     </div>
 
       <div className="offset-xl-6 col-xl-5 col-lg-12 col-md-12 col-12 text-xl-right text-lg-left text-md-left text-left estimate-container">
-           <div style={{borderTop: "1px solid rgba(0,0,0,0.3)", paddingTop: "16px", paddingLeft: "0px", paddingRight: "0px"}}>
-              <div className="container-fluid" style={{paddingLeft: "0px", paddingRight: "0px"}}>
+           <div style={{paddingLeft: "0px", paddingRight: "0px"}}>
+              <div className="container-fluid" style={{paddingTop: "16px", paddingLeft: "0px", paddingRight: "0px", borderTop: "1px solid rgba(0,0,0,0.3)", }}>
                 <div className="row" style={{paddingLeft: "0px", paddingRight: "0px"}}>
                   <div className="col-xl-6 col-lg-6 col-md-6 col-sm-6 col-6" 
                       style={{paddingLeft: "0px"}}>
