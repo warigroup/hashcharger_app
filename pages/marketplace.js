6eb947e5b0a537900f78dd1dcdbe818c5470d17f
webdevstorage <46852738+webdevstorage@users.noreply.github.com>
2020-06-15 13:47:05 -0400

    update --

--- pages/marketplace.js
+++ pages/marketplace.js
@@ -1585,7 +1585,7 @@ class Marketplace extends React.Component {
                       </div>
                             <div className="col-xl-6 col-lg-6 col-md-6 col-sm-6 col-6">
                             <h6 style={{ display: "inline-block", fontSize: "0.9em" }}>
-                              {this.props.estimate.price === undefined ? "--------" : this.props.estimate.price.total_payment_amount} BTC</h6>
+                              {this.props.estimate.price === undefined ? "- - - - - - - - " : this.props.estimate.price.total_payment_amount} BTC</h6>
                             </div>
 
 
@@ -1602,7 +1602,7 @@ class Marketplace extends React.Component {
                             </div>
                             <div className="col-xl-6 col-lg-6 col-md-6 col-sm-6 col-6">
                               <h6 style={{ display: "inline-block", fontSize: "0.9em" }}>
-                              {this.props.estimate.price === undefined ? "--------" : this.props.estimate.price.average_price} BTC
+                              {this.props.estimate.price === undefined ? "- - - - - - - - " : this.props.estimate.price.average_price} BTC
                               </h6>
                             </div>
 
