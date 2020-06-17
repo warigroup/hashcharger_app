7ebfaa1e6590ba3e9624fd93d71efe323c2d6296
webdevstorage <46852738+webdevstorage@users.noreply.github.com>
2020-06-16 20:00:34 -0400

    fix grid 2

--- pages/marketplace.js
+++ pages/marketplace.js
@@ -1575,7 +1575,7 @@ class Marketplace extends React.Component {
 
                     </div>
 
-      <div className="offset-xl-5 col-xl-6 col-lg-12 col-md-12 col-12 text-xl-left text-lg-left text-md-left text-left estimate-container">
+      <div className="offset-xl-6 col-xl-5 col-lg-12 col-md-12 col-12 text-xl-left text-lg-left text-md-left text-left estimate-container">
         <div style={{paddingLeft: "0px", paddingRight: "0px"}}>
           <div className="container-fluid estimate-containerdiv" style={{paddingLeft: "0px", paddingRight: "0px"}}>
           <div className="bordertop"></div>
