16f68de0ad4b4f25504237d8af41303fbaea919d
webdevstorage <46852738+webdevstorage@users.noreply.github.com>
2020-06-26 21:12:07 -0400

    adjust styles

--- pages/marketplace.js
+++ pages/marketplace.js
@@ -1714,7 +1714,7 @@ class Marketplace extends React.Component {
           <div className="container-fluid estimate-containerdiv" style={{paddingLeft: "0px", paddingRight: "0px"}}>
           <div className="bordertop"></div>
             <div className="row rowpaddings">
-              <div className="col-xl-5 col-lg-6 col-md-6 col-sm-5 col-5" 
+              <div className="col-xl-5 col-lg-5 col-md-5 col-sm-5 col-5" 
                 style={{paddingLeft: "3px", paddingRight: "0px"}}>
                     <div className="iconcontainers">
                       <FaBitcoin style={{
@@ -1732,7 +1732,7 @@ class Marketplace extends React.Component {
                       </div>
                             <div className="col-xl-7 col-lg-6 col-md-6 col-sm-7 col-7 text-left">
                             <h6 style={{ display: "inline-block", fontSize: "0.78em" }}>
-                              {this.props.estimate.price === undefined ? "- - - - - - - - " : this.props.estimate.price.total_payment_amount} BTC</h6>
+                              {this.props.estimate.price === undefined ? "- - - - - - - " : this.props.estimate.price.total_payment_amount} BTC</h6>
                             </div>
 
 
@@ -1751,9 +1751,9 @@ class Marketplace extends React.Component {
                               Estimated Rate:
                                </h6>{" "}
                             </div>
-                            <div className="col-xl-7 col-lg-6 col-md-6 col-sm-7 col-7 text-left">
+                            <div className="col-xl-7 col-lg-7 col-md-7 col-sm-7 col-7 text-left">
                               <h6 style={{ display: "inline-block", fontSize: "0.78em" }}>
-                              {this.props.estimate.price === undefined ? "- - - - - - - - " : this.props.estimate.price.average_price} <BTCPaymentRate />
+                              {this.props.estimate.price === undefined ? "- - - - - - - " : this.props.estimate.price.average_price} <BTCPaymentRate />
                               </h6>
                             </div>
 
