5d1082f114a04d07218c5caf95786168ba7e782a
webdevstorage <46852738+webdevstorage@users.noreply.github.com>
2020-07-16 17:59:55 -0400

    format doc

--- pages/_error.js
+++ pages/_error.js
@@ -1,29 +1,30 @@
-import React from "react";
-import {
-  notFoundPage
-} from "../actions/warihashApiCalls";
-import { connect } from "react-redux";
-import { FaArrowLeft } from "react-icons/fa";
-import { Router } from "../routes";
-import PropTypes from "prop-types";
-import PublicRoute from "../components/routes/PublicRoute";
+import React from 'react';
+import { notFoundPage } from '../actions/warihashApiCalls';
+import { connect } from 'react-redux';
+import { FaArrowLeft } from 'react-icons/fa';
+import { Router } from '../routes';
+import PropTypes from 'prop-types';
+import PublicRoute from '../components/routes/PublicRoute';
 
 class NotFound extends React.Component {
-  componentDidMount() {
-    this.props.notFoundPage();
-  }
+	componentDidMount() {
+		this.props.notFoundPage();
+	}
 
- gotomainpage = () => {
-   Router.pushRoute(`/market/${this.props.settings.host}/${this.props.settings.port}/${this.props.settings.username}/${this.props.settings.password}/${this.props.miningalgo.algorithm}`);
- };
+	gotomainpage = () => {
+		Router.pushRoute(
+			`/market/${this.props.settings.host}/${this.props.settings.port}/${this.props.settings.username}/${this
+				.props.settings.password}/${this.props.miningalgo.algorithm}`
+		);
+	};
 
-  render() {
-    return (
-      <PublicRoute>
-        <div style={{ width: "100%", marginBottom: "150px" }}>
-          <div className="container">
-            <style jsx>
-              {`
+	render() {
+		return (
+			<PublicRoute>
+				<div style={{ width: '100%', marginBottom: '150px' }}>
+					<div className="container">
+						<style jsx>
+							{`
                 .gotomain-btn {
                   cursor: pointer;
                   background: ${this.props.theme.primary}
@@ -50,61 +51,56 @@ class NotFound extends React.Component {
                   color: rgba(0, 0, 0, 0.6);
                 }
               `}
-            </style>
-            <div className="row">
-              <div className="col-md-12 col-12 text-center">
-                <br />
-                <br />
-                <br />
-                <br />
-                <br />
-                <br />
-                <br />
-                <h5 className="notfoundtitle">
-                  <strong>An error has occurred :(</strong>
-                </h5>
-                <br />
-                <br />
-                <br />
-
-                <button className="gotomain-btn" onClick={this.gotomainpage}>
-                    <span>
-                      <FaArrowLeft />
-                    </span>{" "}
-                    Go to main page
-                 </button>
-                 
-                <br />
-                <br />
-                <br />
-                <br />
-                <br />
-                <br />
-              </div>
-            </div>
-          </div>
-        </div>
-      </PublicRoute>
-    );
-  }
+						</style>
+						<div className="row">
+							<div className="col-md-12 col-12 text-center">
+								<br />
+								<br />
+								<br />
+								<br />
+								<br />
+								<br />
+								<br />
+								<h5 className="notfoundtitle">
+									<strong>An error has occurred :(</strong>
+								</h5>
+								<br />
+								<br />
+								<br />
+								<button className="gotomain-btn" onClick={this.gotomainpage}>
+									<span>
+										<FaArrowLeft />
+									</span>{' '}
+									Go to main page
+								</button>
+								<br />
+								<br />
+								<br />
+								<br />
+								<br />
+								<br />
+							</div>
+						</div>
+					</div>
+				</div>
+			</PublicRoute>
+		);
+	}
 }
 
 NotFound.propTypes = {
-  notFoundPage: PropTypes.func,
-  profile: PropTypes.object,
-  settings: PropTypes.object,
-  miningalgo: PropTypes.object,
-  theme: PropTypes.object
+	notFoundPage: PropTypes.func,
+	profile: PropTypes.object,
+	settings: PropTypes.object,
+	miningalgo: PropTypes.object,
+	theme: PropTypes.object
 };
 
 const mapStateToProps = state => ({
-  profile: state.profile,
-  settings: state.settings,
-  miningalgo: state.miningalgo,
-  theme: state.theme
+	profile: state.profile,
+	settings: state.settings,
+	miningalgo: state.miningalgo,
+	theme: state.theme
 });
 
-export default connect(
-  mapStateToProps,
-  { notFoundPage }
-)(NotFound);
+export default connect(mapStateToProps, { notFoundPage })(NotFound);
