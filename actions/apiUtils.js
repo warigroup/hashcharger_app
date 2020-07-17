c1b3667fc215c3355cc1c9ecf9fc235795ba6572
webdevstorage <46852738+webdevstorage@users.noreply.github.com>
2020-07-16 21:47:56 -0400

    remove comments

--- actions/apiUtils.js
+++ actions/apiUtils.js
@@ -1,15 +1,9 @@
-//// ADDITIONAL PARSING FOR API DATA ////////////
 
-// adds additional items to entries in get_offer API call
 export function offerAddUtil(offers) {
   let offeritems = offers.result;
   if (offeritems !== undefined ) {
     offeritems.forEach(function(item, index){
-      // this is hashrate that should be displayed to users on table columns
-      // triggered offers do not require hashrate measurements, but depend on declared hashrates
-      // normal offers we display the 24 hour hashrate
       item.display_hashrate = item.triggered ? item.declared_hashrate : item.measured_hashrate['1440'];
-  
     });
   } 
  
