cbda2fd2e6fa3120f8e3034359cf03d54fb0e096
webdevstorage <46852738+webdevstorage@users.noreply.github.com>
2020-07-21 17:25:36 -0400

    update order history page test

--- cypress/integration/orderDetailsPage.spec.js
+++ cypress/integration/orderDetailsPage.spec.js
@@ -7,23 +7,8 @@ describe("Profile page content test", function() {
     it("can render profile page", () => {
       cy.visit("/orderhistory")
       cy.wait(2000)
-      cy.get('p:contains("Username")').should("exist")
-      cy.get('p:contains("Email")').should("exist")
-      cy.get('p:contains("Referral Link")').should("exist")  
-      cy.get('a:contains("Manage Stratum Settings")').should("exist")  
-
-      cy.get('button:contains("Buy Orders")').should("exist").click({force: true})
-      cy.wait(1000)
-      cy.get('h5:contains("My Buy Orders")').should("exist")  
-
-      cy.get('button:contains("Sell Orders")').should("exist") .click({force: true})
-      cy.wait(1000)
-      cy.get('h5:contains("My Sell Orders")').should("exist")  
-
-      cy.get('button:contains("Buy Orders")').should("exist").click({force: true})
-      cy.wait(1000)
-      cy.get('h5:contains("My Buy Orders")').should("exist")     
-     
+      cy.get('h4:contains("My Order History")').should("exist");   
+      cy.get('#tableT').should('exist')
     });
 
 });
