2f58b8267bd586a8932fe9320dfae2cb4a8fefd7
webdevstorage <46852738+webdevstorage@users.noreply.github.com>
2020-07-21 17:23:17 -0400

    update navigation test

--- cypress/integration/Navigation.spec.js
+++ cypress/integration/Navigation.spec.js
@@ -1,82 +1,22 @@
 //Will break these navigation pages upas they expand.
 describe("public pages navigation test", function() {
 
-    it("navigates to terms of use page", () => {
-      cy.visit("/termsofuse");
+    it("navigates to marketplace page", () => {
+      cy.visit("/market/stratum.slushpool.com/3333/widgetaccount/password/sha256d/3626a5/ffffff/3626a5/233f5c/ffffff/ffffff/FRxtvCGmNWV9AqJRKAs7CB/subuser");
       cy.wait(1350);
-      cy.get('h5:contains("Terms of Use")').should("exist");
+      cy.get('h4:contains("Buy Hashing Power for")').should("exist")
     });
   
-    it("navigates to login page", () => {
-      cy.visit("/login");
+    it("navigates order history page", () => {
+      cy.visit("/orderhistory");
       cy.wait(1350);
-      cy.get('h5:contains("Log-in")').should("exist");
+      cy.get('h4:contains("My Order History")').should("exist");
     });
   
-    it("navigates to registration page without referral code", () => {
-      cy.visit("/register");
+    it("navigates to marketplace page", () => {
+      cy.visit("/market/stratum.slushpool.com/3333/widgetaccount/password/sha256d/3626a5/ffffff/3626a5/233f5c/ffffff/ffffff/FRxtvCGmNWV9AqJRKAs7CB/subuser");
       cy.wait(1350);
-      cy.get('h5:contains("Sign Up")').should("exist");
-    });
-
-    it("navigates to registration page with referral code", () => {
-      cy.visit("/register/8gf38fu83uf8");
-      cy.wait(1350);
-      cy.get('h5:contains("Sign Up")').should("exist");
-    });
-  
-    it("navigates to password reset page", () => {
-      cy.visit("/passwordreset");
-      cy.wait(1350);
-      cy.get('h5:contains("Reset your password")').should("exist");
-    });
-  
-    it("navigates to resend page", () => {
-      cy.visit("/resend");
-      cy.wait(1350);
-      cy.get('h5:contains("Resend activation link")').should("exist");
-    });
-
-    it("navigates to market history", () => {
-      cy.visit("/markethistory")
-      cy.wait(1350);
-      cy.get('h4:contains("Market History for")').should("exist")
-    });
-
-    it("navigates to marketplace without algo parameter", () => {
-        // go to root url
-        cy.visit("/")
-        cy.wait(1350);
-        cy.get('h4:contains("Buy Hashing Power for")').should("exist");
-
-        // go to marketplace url
-        cy.visit("/marketplace")
-        cy.wait(1350);
-        cy.get('h4:contains("Buy Hashing Power for")').should("exist");
-    });
-
-    it("navigates to marketplace with algo parameter", () => {
-      // go to sha256d url
-      cy.visit("/marketplace/sha256d")
-      cy.wait(1350);
-      cy.get('h4:contains("Buy Hashing Power for")').should("exist");
-      // go to scrypt url
-      cy.visit("/marketplace/scrypt")
-      cy.wait(1350);
-      cy.get('h4:contains("Buy Hashing Power for")').should("exist");
-      // go to ethash url
-      cy.visit("/marketplace/ethash")
-      cy.wait(1350);
-      cy.get('h4:contains("Buy Hashing Power for")').should("exist");
-
-    });
-
-    it("redirects users to a 404 error page when users enter wrong algorithm on marketplace url", () => {
-      // go to non-existing url
-      cy.visit("/marketplace/daggerhashmoto")
-      cy.wait(1500);
-      cy.get('h5:contains("ERROR: Page Not Found")').should("exist");
-
+      cy.get('h4:contains("Buy Hashing Power for")').should("exist")
     });
 
   });
